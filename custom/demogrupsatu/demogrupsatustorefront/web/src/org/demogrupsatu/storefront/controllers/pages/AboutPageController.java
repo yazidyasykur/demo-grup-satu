6aaec7d137f62e9123b716529eb7c534bd6f7fc0
Yazid Yasykur <yasykur@gmail.com>
2022-12-06 16:28:12 +0700

    delete testimoni

--- custom/demogrupsatu/demogrupsatustorefront/web/src/org/demogrupsatu/storefront/controllers/pages/AboutPageController.java
+++ custom/demogrupsatu/demogrupsatustorefront/web/src/org/demogrupsatu/storefront/controllers/pages/AboutPageController.java
@@ -13,12 +13,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/about")
 public class AboutPageController extends AbstractPageController {
-
-    private static final String TEMPLATE_PAGE = "aboutPage";
-
+    private static final String TEMPLATE_PAGE = "about";
     @RequestMapping(value=TEMPLATE_PAGE, method = RequestMethod.GET)
     public String getAbout(final Model model) throws CMSItemNotFoundException{
-        System.out.println("Hello");
         final ContentPageModel demoData = getContentPageForLabelOrId(TEMPLATE_PAGE);
         storeCmsPageInModel(model, demoData);
         setUpMetaDataForContentPage(model, demoData);
